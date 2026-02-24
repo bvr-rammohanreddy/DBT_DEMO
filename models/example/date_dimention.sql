d6f3e9a60004705abe9ddd55c50822831a7d802e
Rammohan Reddy <b.ramoracle@gmail.com>
2026-02-24 12:13:59 +0000

    macro_exapmles

--- models/example/date_dimention.sql
+++ models/example/date_dimention.sql
@@ -9,8 +9,10 @@ case when dayname(to_timestamp(STARTED_AT)) in ('Sat','Sun') then 'Weekend' else
 case when month(to_timestamp(STARTED_AT)) in (12,1,2) then 'Winter'
      when month(to_timestamp(STARTED_AT)) in (3,4,5) then 'Sring'
      when month(to_timestamp(STARTED_AT)) in (6,7,8) then 'Summer'
-     else 'Autumn' end as "stationOfYear"  
-     
+     else 'Autumn' end as "stationOfYear"  ,
+     {{fun1('STARTED_AT')}} as f1,
+     {{get_season('STARTED_AT')}} as get_season,
+     {{day_type('STARTED_AT')}} as day_type_fun,
 
 
 from {{ source('demo', 'bike') }}
